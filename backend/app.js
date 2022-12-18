e731dd749ee3dcfb48591cdb01764331876e0d70
Ethan Graham <ethangraham43@gmail.com>
2022-12-18 18:59:42 -0500

    express error handling

--- backend/app.js
+++ backend/app.js
@@ -44,4 +44,25 @@ app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
 
-module.exports = app;
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+  
+  const serverErrorLogger = debug('backend:error');
+  
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+  app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
+  
+  module.exports = app;
