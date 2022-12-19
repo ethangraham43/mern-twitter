12ab194f81f3fb83d9913316a95342a4e854597f
Ethan Graham <ethangraham43@gmail.com>
2022-12-18 22:54:44 -0500

    added tweets frontend

---
+++ frontend/src/components/Tweets/TweetBox.js
@@ -0,0 +1,10 @@
+
+function TweetBox ({ text, username }) {
+    return (
+      <div className="tweet">
+        <h3>{username ? `${username}:` : ""} {text} </h3>
+      </div>
+    );
+  }
+  
+  export default TweetBox;
